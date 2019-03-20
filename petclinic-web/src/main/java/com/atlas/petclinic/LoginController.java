e5b066a88fb650101904725b38a20dffc14c5e45
Sandeep Mantha <sandeep.mantha@anthem.com>
2019-03-20 09:54:08 -0400

    error page

--- petclinic-web/src/main/java/com/atlas/petclinic/LoginController.java
+++ petclinic-web/src/main/java/com/atlas/petclinic/LoginController.java
@@ -3,12 +3,15 @@
  */
 package com.atlas.petclinic;
 
+import java.util.Map;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 
 /**
@@ -22,6 +25,12 @@ public class LoginController {
     public void login(HttpServletRequest request, HttpServletResponse response) throws Exception {
     	response.sendRedirect("/petclinic/#/h/petclinicdashboard/vpDashboard");
         
+    }
+    
+
+    @RequestMapping(value = "/apperror", method = RequestMethod.GET)
+    public String error(@RequestParam Map<String,String> allParams, HttpServletRequest request, HttpServletResponse response) throws Exception {
+    	return "customerror";
     }
 
 }
